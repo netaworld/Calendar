994c318ddea893e11df2128c7d80ff8bde701826
Aqua <aqua@Aquas-MacBook-Pro.local>
2019-10-28 10:00:44 -0400

    local storage

---
+++ Assets/script.js
@@ -0,0 +1,15 @@
+
+
+// save to local storage//
+$(document).ready(function(){
+    $(".btn-save").on("click",function(){
+  console.log("it saved it")
+    })
+
+
+    // localStorage.setItem('myCat', 'Tom');
+
+})
+//get out from storage//
+
+//colors are changing by time//
